ad93e6b9ba13e08999dd5d13a26b3d8a16fc975f
Olga Rudson <o.rudson@mail.ru>
2022-10-16 19:20:18 +0300

    Четные числа от 1 до N

---
+++ example004/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine() ??""; 
+int N = int.Parse(numberStr);
+
+int i = 0;
+while(i < (N - 1))
+{
+    i += 2;
+    Console.WriteLine(i);
+}
